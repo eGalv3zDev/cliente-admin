d98a801e5dbeee0d10491bf06918437ddb6faf78
Mario Alejandro Escobar Silva <acamposeco-2024018@kinal.edu.gt>
2026-04-07 10:21:06 -0600

    inicio de sesion:

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,12 @@
-const App = () => {
+import { AuthPage } from "../features/auth/pages/AuthPage";
+
+
+function App() {
+
   return(
     <>
-      <h1 className="">Hola mundo</h1>
-      <p>Lorem ipsum, dolor sit amet consectatur
-        adipisicing elit. Enim deleniti voluptates
-        dignissimos accusamus minus earum eum optio 
-        cumque impedit labore ero ipsa tempora
-      </p>
+      <h1>Hola mundo</h1>
+      <AuthPage />
     </>
   )
 }
