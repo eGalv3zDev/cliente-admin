f604e77ca64372279d549b52422afd2c8a1b86d0
Mario Alejandro Escobar Silva <acamposeco-2024018@kinal.edu.gt>
2026-04-07 07:18:29 -0600

    primer commit

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return(
+    <>
+      <h1 className="">Hola mundo</h1>
+      <p>Lorem ipsum, dolor sit amet consectatur
+        adipisicing elit. Enim deleniti voluptates
+        dignissimos accusamus minus earum eum optio 
+        cumque impedit labore ero ipsa tempora
+      </p>
+    </>
+  )
+}
+
+export default App;
