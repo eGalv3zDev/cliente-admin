7d97deffb5482ad17f9b5476c36df89594705726
Mario Alejandro Escobar Silva <acamposeco-2024018@kinal.edu.gt>
2026-04-15 10:42:36 -0600

    feat/ Cliente-admin 15/04/26

--- src/app/router/AppRoutes.jsx
+++ src/app/router/AppRoutes.jsx
@@ -1,16 +1,24 @@
-import {Route, Routes} from "react-router-dom";
-import { AuthPage } from "../../features/auth/pages/AuthPage";
-import { DashboardPage } from "../layaouts/DashboardPage";
+import { Routes, Route } from "react-router-dom";
+import { AuthPage } from "../../features/auth/pages/AuthPage.jsx";
+import { DashboardPage } from "../layouts/DashboardPage.jsx";
 
-export const AppRoutes = () => {
+export const AppRoutes = ()=> {
+
     return(
         <Routes>
+
             {/* PUBLIC */}
-            <Route path="/" element={<AuthPage />} />
+            <Route path="/" element={<AuthPage/>} />
 
-            {/* PROTEGIDO POR ROLE */}
-            <Route path="/dashboard/*" element={<DashboardPage />} />
 
+            {/* PROTECTED + ROLE */}
+            <Route
+                path="/dashboard/*" element={<DashboardPage />}
+            />
+
+
+            {/* Ruta temporal para pruebas */}
+            <Route path="*" element={<h1>Página no encontrada</h1>} />
         </Routes>
     );
-};
+}
