c8b24fc077de94c39ccf814ce883ce247cba4a70
Adrian Camposeco <acamposeco-2024018@kinal.edu.gt>
2026-04-15 10:52:10 -0600

    fix/ se arreglo problema con caropetas

--- src/app/layaouts/DashboardPage.jsx
+++
@@ -1,7 +0,0 @@
-import { DashBoardContainer } from "../../shared/components/layaout/DashBoardContainer";
-
-export const DashboardPage = ()=> {
-    return(
-        <DashBoardContainer />
-    );
-}
